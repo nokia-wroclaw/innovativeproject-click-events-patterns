765025aa3db8a1da8fca4162c835de2f177046de
Roling551 <Roling95@gmail.com>
2018-04-17 17:58:46 +0200

    correcting plots

--- notebooks/Plots.py
+++ notebooks/Plots.py
@@ -11,7 +11,7 @@ import functools
 import bokeh
 from bokeh.core.properties import value
 from bokeh.io import show, output_file
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import figure
 from bokeh.io import output_notebook
 from collections import Counter
@@ -23,15 +23,14 @@ output_notebook()
 PLOT_WIDTH=900
 PLOT_HEIGHT=750
 
-
-
+    
 
 def plot(series, title="",y_axis_label="ilość"):
     p = figure(title=title, x_axis_label=series.index.name, y_axis_label='ilość', x_range=series.index.values, plot_width=PLOT_WIDTH, plot_height=PLOT_HEIGHT)
     p.xaxis.major_label_orientation = math.pi/5
     p.vbar(series.index.values, line_width=2,top=series.values,width=0.9)
     p.xaxis.major_label_text_font_size = "9pt"
-    show(p)
+    return p
 
 
 
@@ -43,7 +42,7 @@ def plotUniquePerMonth(data, columnName):
     p.xaxis.major_label_orientation = math.pi/5
     p.vbar(amountPerMonth.index.values, line_width=2,top=amountPerMonth.values,width=0.9)
     p.xaxis.major_label_text_font_size = "9pt"
-    show(p)
+    return p
 
 
 
@@ -55,7 +54,7 @@ def plotAmountPerMonth(data):
     p.xaxis.major_label_orientation = math.pi/5
     p.vbar(amountPerMonth.index.values, line_width=2,top=amountPerMonth.values,width=0.9)
     p.xaxis.major_label_text_font_size = "9pt"
-    show(p)
+    return p
 
 
 
@@ -67,7 +66,7 @@ def plotUniquePerMonthPercent(data, data2, columnName):
     p.xaxis.major_label_orientation = math.pi/5
     p.vbar(amountPerMonth.index.values, line_width=2,top=amountPerMonth.values,width=0.9)
     p.xaxis.major_label_text_font_size = "9pt"
-    show(p)
+    return p
 
 
 
@@ -79,7 +78,42 @@ def plotAmountPerMonthPercent(data, data2):
     p.xaxis.major_label_orientation = math.pi/5
     p.vbar(amountPerMonth.index.values, line_width=2,top=amountPerMonth.values,width=0.9)
     p.xaxis.major_label_text_font_size = "9pt"
-    show(p)
+    return p
 
+#    g = data.groupby([col2, col1]).size()
+def squaredGraph(doubleGroupBy):
+    xAxis = []
+    yAxis = []
+    fields = []
 
+    g1=doubleGroupBy.unstack()
+    g1=g1.fillna(0.0)
+    for x in g1:
+        for y_name, y in g1[x].iteritems():
+            yAxis.append(x)
+            fields.append(y)
+            xAxis.append(y_name)
+    colors = []
+    maximum = max(fields)
+    for c in fields:
+        colors.append(bokeh.colors.RGB(255,(1-math.sqrt(c/maximum))*255,255))
+    desc = list(map(lambda x:str(x),fields))
+    source = ColumnDataSource(data=dict(
+        y = yAxis, 
+        x= xAxis,
+        color=colors,
+        desc = desc
+    ))
+    hover = HoverTool(tooltips=[
+        ("department", "@y"),
+        ("tool", "@x"),
+        ("numbers", "@desc")
+    ])
+    hm = figure(title="Action count", tools=[hover,  'wheel_zoom', 'box_zoom', 'pan', 'reset'],
+                y_range=list(doubleGroupBy.index.levels[1]), x_range=list(doubleGroupBy.index.levels[0]),  plot_width=600, plot_height=600)
+
+    hm.rect(     y = 'y', x = 'x', source = source, color="color", width=1, height=1)
+    hm.xaxis.major_label_orientation = math.pi/3
+
+    return hm 
 
